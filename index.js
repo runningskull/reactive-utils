c139137fc73e4088470c8b8eda04fc45f38fc479
J.R. Patten <jr@juanpatten.com>
2014-07-31 19:50:17 -0600

    Add remove-if

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = function (template_or_el, model, options) {
   options = options || {}
   options.bindings = options.bindings || {}
   options.bindings['keep-if'] = keepIf
+  options.bindings['remove-if'] = removeIf
 
   if (options.partials) {
     foreach(options.partials, function (View, name) {
@@ -39,6 +40,29 @@ function partial (Partial) {
 }
 
 
+/* Removes the element from a page if a property is true.
+ * Opposite of keepIf.
+ *
+ *  Example:
+ *      `<span class="profile-data" keep-if="hasProfile">`
+ *  
+ */
+function removeIf(el, property) {
+  var binding = this
+    , parent = el.parentNode
+
+  binding.change(function() {
+    var value = binding.value(property)
+    console.log('keep-if', property, value, el, el.parentNode, parent)
+    if (value && el.parentNode) {
+      el.parentNode.removeChild(el)
+    } else if (value && !el.parentNode) {
+      parent.appendChild(el)
+    }
+  });
+};
+
+
 /* Keeps the element on a page if a property is true.
  * Opposite of removeIf.
  *
@@ -46,7 +70,7 @@ function partial (Partial) {
  *      `<span class="profile-data" keep-if="hasProfile">`
  *  
  */
-function keepIf(el, property){
+function keepIf(el, property) {
   var binding = this
     , parent = el.parentNode
 
@@ -60,3 +84,30 @@ function keepIf(el, property){
     }
   });
 };
+
+
+/* Keeps the element on a page if model[property] == value
+ * Uses double-equal comparison.
+ *
+ *  Example:
+ *      `<span class="propertyEditor" keep-if-equal="dataType=boolean">`
+ *  
+ */
+function keepIfEqual(el, attrVal) {
+  var spl = attrVal.split(/ ?= ?/)
+    , property = spl[0]
+    , test = spl[1]
+
+  var binding = this
+    , parent = el.parentNode
+
+  binding.change(function() {
+    var value = binding.value(prop)
+    if ((value == test) && el.parentNode) {
+      el.parentNode.removeChild(el)
+    } else if (value && !el.parentNode) {
+      parent.appendChild(el)
+    }
+  })
+}
+
